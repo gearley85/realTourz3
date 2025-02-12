5627f3c2b3135f4921ad8ac966f3118008708366
gavinearley <gavinearley@outlook.com>
2025-02-12 06:32:02 -0800

    Adding Google verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,24 +1,55 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="keywords" content="React Nextjs Template Geekfolio Multi-Purpose themeforest" />
-        <meta name="description" content="Geekfolio - Multi-Purpose React.js Next.js Template" />
-        <meta name="author" content="" />
+        <meta
+          name="keywords"
+          content="Realtourz drone photography aerial images pcs move"
+        />
+        <meta
+          name="description"
+          content="Realtourz the new way to tour homes"
+        />
+        <meta name="author" content="Gavin Earley" />
+        <meta
+          name="google-site-verification"
+          content="KICzyyUF1qS5nMkYNh4AofLJLlqQ7ZlCEvJ_Sdnr9_8"
+        />
         {/* ------ Favicon ------ */}
         <link rel="shortcut icon" href="/dark/assets/imgs/favicon.ico" />
         {/* ------ Google Fonts ------ */}
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Epilogue:wght@100;200;300;400;500;600;700;800;900&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Epilogue:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        />
         {/* ------ bootstrap icons cdn ------ */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+        />
         {/* ------ Plugins ------ */}
         <link rel="stylesheet" href="/dark/assets/css/plugins.css" />
         {/* ------ Core Style Css ------ */}
@@ -29,5 +60,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
